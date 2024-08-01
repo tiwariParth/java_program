0557c62359f802b29110b4face9f665d53ff2945
tiwariParth <243mjparth@gmail.com>
2024-08-01 16:33:21 +0530

    feat: Add switch case program to determine if a number is even or odd

---
+++ SwitchCase/SwitchCaseExp.java
@@ -0,0 +1,21 @@
+package SwitchCase;
+
+import java.util.Scanner;
+
+public class SwitchCaseExp {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    switch (a%2) {
+        case 0:
+                System.out.println("even");
+            break;
+        case 1:
+            System.out.println("odd");
+            break;
+        default:
+            System.out.println("No number");
+            break;
+    }
+   }
+}
