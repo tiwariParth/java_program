5449f50294b2e3553161ea6f50b205e14c5932e4
tiwariParth <243mjparth@gmail.com>
2024-08-03 16:07:49 +0530

    refactor: Improve pattern printing logic

--- pattern/Pattern.java
+++ pattern/Pattern.java
@@ -2,9 +2,9 @@ package pattern;
 
 public class Pattern {
    public static void main(String[] args) {
-         for (int i = 1; i <= 5; i++) {
-              for (int j = 1; j <= i; j++) {
-                System.out.print(" "+ j);
+         for (int i = 1; i <= 10; i++) {
+              for (int j = 1; j <= 10; j++) {
+                System.out.print(" "+i*j);
               }
               System.out.println();
          }
---
+++ pattern2/pattern2.java
@@ -0,0 +1,11 @@
+package pattern2;
+
+public class pattern2 {
+    public static void main(String[] args) {
+        for(int i=0;i<3;i++){
+            for(int j=0;j<2;j++){
+                System.out.println(i+" "+j+" ");
+            }
+        }
+    }
+}
