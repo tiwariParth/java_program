8b588259943b72457dc115eb912e33ff1f637482
tiwariParth <243mjparth@gmail.com>
2024-08-05 15:45:53 +0530

    Refactor MaxMinArray.java to improve code logic and remove unnecessary code

--- MaxMinArray/MaxMinArray.java
+++ MaxMinArray/MaxMinArray.java
@@ -13,11 +13,14 @@ public class MaxMinArray {
             arr[i] = sc.nextInt();
             max = arr[i];
             min = arr[i];
-            if (arr[i]>max) {
-                max = arr[i];
+
+        }
+        for(int j = 0;j<=arr.length-1;j++){
+            if (arr[j]>max) {
+                max = arr[j];
             }
-            if (arr[i]<min) {
-                min = arr[i];
+            if (arr[j]<min) {
+                min = arr[j];
             }
         }
         System.out.println("Max number is :"+ max);
