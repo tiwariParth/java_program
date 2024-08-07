1f4e10a29825063e74a5032966f73c10d5e873ae
tiwariParth <243mjparth@gmail.com>
2024-08-07 16:36:55 +0530

    Refactor CountString.java for consistent spacing and code cleanup

--- StringCount/CountString.java
+++ StringCount/CountString.java
@@ -2,7 +2,7 @@ package StringCount;
 
 import java.util.Scanner;
 
-public class CountString {
+public class CountString{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String s = sc.nextLine();
